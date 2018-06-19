178a20ac75d7fa9cbe43b68aec1712692b0e6b7f
Jordi Burgos <jordiburgos@gmail.com>
2018-06-19 23:42:08 +0100

    Improve camera capture.

--- predict_cam.py
+++ predict_cam.py
@@ -19,16 +19,6 @@ def prepare_image(img):
     return grey_values
 
 def main():
-    X = np.empty((10, 28, 28))
-    for i in range(0,10):
-        print(i)
-        input_image = cv2.imread("./data/test-"+ str(i) +".png", cv2.IMREAD_GRAYSCALE)
-        X[i] = prepare_image(input_image)
-
-    #reshape
-    print('X', X.shape)
-    X = X.reshape(X.shape[0], X.shape[1], X.shape[2], 1)
-    print('X', X.shape)
 
     # load json and create model
     json_file = open('./output/mnist_model.json', 'r')
@@ -40,33 +30,33 @@ def main():
     loaded_model.load_weights("./output/mnist_model.h5")
     print("Loaded model from disk")
     
-    predictions = loaded_model.predict(X)
-
-    print('pred', predictions.shape)
-    print(predictions)
-    for p in predictions:
-        print(np.argmax(p))
-
+    print('Enabling camera...')
     cap = cv2.VideoCapture(0)
+    print('Camera enabled:', cap)
+
     while True:
         ret, img = cap.read()
+        if not ret:
+            print('ret', ret)
+            print('img', img)
+        else:
+            gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            (thresh, bw_img) = cv2.threshold(gray_img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+            X = prepare_image(bw_img)
+            X = X.reshape(1, X.shape[0], X.shape[1], 1)
+            prediction = loaded_model.predict(X)
+            num = np.argmax(prediction)
+            print('prediction', num)
 
-        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        (thresh, bw_img) = cv2.threshold(gray_img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        X = prepare_image(bw_img)
-        X = X.reshape(1, X.shape[0], X.shape[1], 1)
-        prediction = loaded_model.predict(X)
-        num = np.argmax(prediction)
-        print('prediction', num)
-
-        cv2.imshow("input", bw_img)
+            cv2.imshow("camera", img)
+            cv2.imshow("b/w", bw_img)
 
         key = cv2.waitKey(500)
         if key == 27:
             break
 
+    cap.release()
     cv2.destroyAllWindows()
-    cv2.VideoCapture(0).release()
 
 if __name__ == '__main__':
     main()
